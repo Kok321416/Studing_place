33919fa0cf2ec9c6265af443870ba63b0c3689ab
Kok321416 <kok321416xxx@yandex.ru>
2025-08-26 19:50:40 +0800

    Домашка 30.2 c правками

--- courses/serializers.py
+++ courses/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
@@ -12,8 +18,3 @@ class CourseSerializer(serializers.ModelSerializer):
     
     def get_lessons_count(self, obj):
         return obj.lessons.count()
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
